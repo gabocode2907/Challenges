75688ba6bed9a900b84a0d2397c7c66019a836ff
gabocode2907 <83044398+gabocode2907@users.noreply.github.com>
2021-04-28 21:48:55 -0500

    Update

--- imprimeHasta.js
+++ imprimeHasta.js
@@ -1,8 +1,11 @@
 function imprimeHasta(x) {
+    if(x<1){
+        return false;
+    }
     for(var i = 0 ; i <= x ; i++){
         console.log(i);
     }
 }
-imprimeHasta(1000000); // debe imprimir todos los enteros desde el 1 hasta el 1000000
-y = imprimeHasta(-10); 
+imprimeHasta(100); // debe imprimir todos los enteros desde el 1 hasta el 1000000
+var y = imprimeHasta(-10); 
 console.log(y); // debe imprimir 'false'just a test testing a lot
